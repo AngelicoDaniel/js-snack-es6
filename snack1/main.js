7be8e1d4f2151f9c8d06f674c11884f637a5950c
Daniel <angelicodaniel93@gmail.com>
2022-11-07 14:08:02 +0100

    snak1 da rivedere, inizio snack2

--- snack1/main.js
+++ snack1/main.js
@@ -4,3 +4,15 @@
 // Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 // Eseguiamo questo esercizio prima con forEach e poi con filter.
 // esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+let min = 2
+let max = 5
+
+//filter
+const arrFilter = myArray.filter((numero) => {
+    if (numero >= min && numero <= max) {
+        return true
+    }
+})
+console.log(arrFilter)
