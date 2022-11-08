a70ef6cbbf93459ebf5ae2daacfc6c0edbd74588
Daniel <angelicodaniel93@gmail.com>
2022-11-08 15:21:02 +0100

    creati array

--- snack3/main.js
+++ snack3/main.js
@@ -1,2 +1,23 @@
 // Snack 3
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bici = [
+    {
+        nome: 'Monociclo',
+        peso: 1
+    },
+
+    {
+        nome: 'Bicicletta',
+        peso: 1.5
+    },
+
+    {
+        nome: 'Triciclo',
+        peso: 2
+    }
+]
+
+
+let {peso} = PesoBici
+console.log(Math.min(PesoBici))
